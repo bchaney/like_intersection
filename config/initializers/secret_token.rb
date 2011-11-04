2cddf06b472d471c10c0a99b878537f74f2bf59a
Bill Chaney <wchaney2010@gmail.com>
2011-11-04 12:21:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LikeIntersection::Application.config.secret_token = 'd32d13a66e0a200b7ef1adb542813166dca8515280b98948024ce53709842d7ce5148a22853c6928499bbbe3090c6fb2b903954210af6b8c74ee29b52c798afd'
