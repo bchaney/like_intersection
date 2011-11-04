7f59f27abd667760fc2be4a861987212de09b6d9
Bill Chaney <wchaney2010@gmail.com>
2011-11-04 16:28:36 -0500

    fixed a koala deprecation issue

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,11 +8,11 @@ class UserController < ApplicationController
         @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
         if @facebook_cookies != nil
             @access_token = @facebook_cookies["access_token"]
-            @graph = Koala::Facebook::GraphAPI.new(@access_token)
-            @me, @friends, @likes = @graph.batch do |batch_api|
+            @api = Koala::Facebook::API.new(@access_token)
+            @me, @friends, @likes = @api.batch do |batch_api|
                 batch_api.get_object('me')
-                batch_api.get_connections('friends')
-                batch_api.get_connections('likes')
+                batch_api.get_connections('me', 'friends')
+                batch_api.get_connections('me', 'likes')
             end
         else
             redirect_to login_path 
