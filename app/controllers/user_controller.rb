f0076223aefcb29db5836463cb2f339319e9817c
Bill Chaney <wchaney2010@gmail.com>
2011-11-04 16:02:23 -0500

    added basic fb authentication, and tries to get user profile, friends and user likes and list all friends

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,25 @@
 class UserController < ApplicationController
-  def login
-      @title = "Login"
-  end
+    def login
+        @title = "Login"
+    end
 
-  def friends
-      @title = "Friends"
-  end
+    def friends
+        @title = "Friends"
+        @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+        if @facebook_cookies != nil
+            @access_token = @facebook_cookies["access_token"]
+            @graph = Koala::Facebook::GraphAPI.new(@access_token)
+            @me, @friends, @likes = @graph.batch do |batch_api|
+                batch_api.get_object('me')
+                batch_api.get_connections('friends')
+                batch_api.get_connections('likes')
+            end
+        else
+            redirect_to login_path 
+        end
+    end
 
-  def likes
-      @title = "Likes"
-  end
-
+    def likes
+          @title = "Likes"
+    end
 end
