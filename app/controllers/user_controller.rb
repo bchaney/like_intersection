c95486de53fea3a2674a34594184f94f30990fa9
Bill Chaney <wchaney2010@gmail.com>
2011-11-05 17:29:39 -0500

    intersection of user's likes and a friends likes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,7 @@
 class UserController < ApplicationController
     before_filter :parse_facebook_cookies
+    @api
+    @likes
 
     def parse_facebook_cookies
         @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
