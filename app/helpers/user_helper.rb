e9a9104ac4d673f554b76e1f0611115ee289e413
Bill Chaney <wchaney2010@gmail.com>
2011-11-06 12:55:25 -0600

    switched to using tables, moved adding a friend into a helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,11 @@
 module UserHelper
+    def add_friend(friend) 
+        html = ""
+        html << "<td>"
+        html << "#{link_to(image_tag("http://graph.facebook.com/" + 
+                                        friend["id"] + "/picture", 
+                                        :class => "round"), 
+                                        likes_path(friend), friend)}"
+        html << "<br>#{link_to(friend["name"], likes_path(friend), friend)}"
+        html << "</td>
 end
