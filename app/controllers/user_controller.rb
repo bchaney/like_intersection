895875aa152c20b463bbab4f583d40d682a7c719
Bill Chaney <wchaney2010@gmail.com>
2011-11-06 01:35:21 -0500

    sort friends by first name

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,7 +20,7 @@ class UserController < ApplicationController
                 batch_api.get_object('me')
                 batch_api.get_connections('me', 'friends')
             end
-            @friends.sort_by { |item| item["name"] }
+            @friends.sort_by { |k, v| v["name"] }
         else
             redirect_to login_path 
         end
