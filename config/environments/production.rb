97c78b437fd63c49960844218b3a68d8250977c5
Bill Chaney <wchaney2010@gmail.com>
2011-11-05 22:55:53 -0500

    assets moved

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,6 @@ LikeIntersection::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-    # Compress both stylesheets and JavaScripts
-    config.assets.js_compressor  = :uglifier
-    config.assets.css_compressor = :scss
   
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
