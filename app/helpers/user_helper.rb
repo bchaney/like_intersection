1a1fc5829cfb96eb53e58879a03eff9c98c4f445
Bill Chaney <wchaney2010@gmail.com>
2011-11-07 00:17:07 -0600

    styling login page

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -8,15 +8,11 @@ module UserHelper
         text_link = link_to(friend["name"], likes_path(friend), friend)
         # Create a link to the profile picture and like instersection link
         data << img_link
-        #link_to(image_tag("http://graph.facebook.com/" + 
-        #                                friend["id"] + "/picture", 
-        #                                :class => "round"), 
-        #                                likes_path(friend), friend)}"
         data << "<br>#{text_link}"
-        #data << "<br>#{link_to(friend["name"], likes_path(friend), friend)}"
         data << "</td>"
         data.html_safe
     end
+
     def add_like(like)
         data = ""
         data << "<td>"
